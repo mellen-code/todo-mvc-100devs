06f444612a7611f3a55e6b4035389f9666f79ae7
MSmith <marie.ellen@gmail.com>
2023-01-16 11:15:37 -0500

    Added node modules and updated .env

--- config/database.js
+++ config/database.js
@@ -2,14 +2,12 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
+    const conn = await mongoose.connect(process.env.DB_STRING)
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (err) {
+  } 
+  
+  catch (err) {
     console.error(err)
     process.exit(1)
   }
